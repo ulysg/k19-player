4039cd2554f2bb1ff87b4bbd27c21f999f874c63
ulysg <compte@ulys.ch>
2024-04-04 11:03:52 +0200

    Fixed my gitignore lol

--- lib/data/constants.dart
+++
@@ -1,6 +0,0 @@
-const String urlServer = "https://demo.navidrome.org";
-const String userSubsonic = "demo";
-const String passwordSubsonic = "demo";
-const String saltSubsonic = "salt";
-const String versionSubsonic = "1.15.1";
-const String appnameSubsonic = "k19-player";
