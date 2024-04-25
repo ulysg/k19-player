257502ee4e1546a302a4a922d9f9737d73eaf61e
ping <dev@guillaumepin.ch>
2024-04-25 13:58:54 +0200

    Add a connection class to manage different servers

---
+++ lib/domain/entities/connection.dart
@@ -0,0 +1,35 @@
+import 'package:crypto/crypto.dart';
+import 'dart:convert';
+
+class Connection {
+  final String url;
+  final String username;
+  final String passwordHash;
+  final String salt;
+  final String version;
+  final String appname;
+
+  Connection(this.url, this.username, String password, this.salt, this.version,
+      this.appname)
+      : passwordHash = _hashPassword(password, salt);
+
+  static String _hashPassword(String password, String salt) {
+    return md5.convert(utf8.encode(password + salt)).toString();
+  }
+
+  factory Connection.fromJson(Map<String, dynamic> json) {
+    return Connection(json["url"], json["username"], json["passwordHash"],
+        json["salt"], json["version"], json["appname"]);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "url": url,
+      "username": username,
+      "passwordHash": passwordHash,
+      "salt": salt,
+      "version": version,
+      "appname": appname,
+    };
+  }
+}
