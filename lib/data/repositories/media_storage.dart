d48eee2524d8fc117dd39ff0bf420616505e825d
ping <dev@guillaumepin.ch>
2024-05-15 19:25:41 +0200

    quick fix img

--- lib/data/repositories/media_storage.dart
+++ lib/data/repositories/media_storage.dart
@@ -1,4 +1,6 @@
 import 'dart:io';
+import 'dart:typed_data';
+import 'package:image/image.dart' as img;
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 
@@ -14,12 +16,27 @@ class MediaStorage {
     await img.writeAsBytes(response.bodyBytes);
   }
 
+  static Future<bool> isImage(File file) async {
+    try {
+      Uint8List bytes = await file.readAsBytes();
+      img.Image? decodedImage = img.decodeImage(bytes);
+      return decodedImage != null;
+    } catch (e) {
+      return false; // Error occurred during decoding
+    }
+  }
+
   static Future<Uri> getImage(String name) async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
     File img = File("${documentsDirectory.path}/$name");
 
     if (await img.exists()) {
-      return img.uri;
+      bool isImageFile = await isImage(img);
+      if (isImageFile) {
+        return img.uri;
+      } else {
+        throw Exception("File is not a valid image");
+      }
     }
     throw Exception("Error during file retrieval");
   }
